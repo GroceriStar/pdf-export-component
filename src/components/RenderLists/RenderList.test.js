130ac5379c918fa183170344282f6ba1083cebc2
Matheus Souza <37983247+souzasmatheus@users.noreply.github.com>
2019-02-07 16:29:31 -0200

    Update RenderList.test.js

--- src/components/RenderLists/RenderList.test.js
+++ src/components/RenderLists/RenderList.test.js
@@ -1,10 +1,31 @@
 import React    from 'react';
-import RenderList from './RenderLists2.js';
+import RenderList1 from './RenderLists1.js';
+import RenderList2 from './RenderLists2.js';
+import RenderList3 from './RenderLists3.js';
+import RenderList4 from './RenderLists4.js';
 import Enzyme, { shallow } from 'enzyme';
 
 describe('Test RenderList attribute', () => {
-  it('Renderlist has data attribute', () => {
-    const renderList = shallow(<RenderList />);
+  it('Renderlist1 has data attribute', () => {
+    const renderList = shallow(<RenderList1 data={data}/>);
+    const data = renderList.node.props.data;
+    expect(data).toBe('data')
+  })
+  
+  it('Renderlist2 has data attribute', () => {
+    const renderList = shallow(<RenderList2 data={data}/>);
+    const data = renderList.node.props.data;
+    expect(data).toBe('data')
+  })
+  
+  it('Renderlist3 has data attribute', () => {
+    const renderList = shallow(<RenderList3 data={data}/>);
+    const data = renderList.node.props.data;
+    expect(data).toBe('data')
+  })
+  
+  it('Renderlist4 has data attribute', () => {
+    const renderList = shallow(<RenderList4 data={data}/>);
     const data = renderList.node.props.data;
     expect(data).toBe('data')
   })
