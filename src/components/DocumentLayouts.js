77c492a1b5f03eeee4a30be884447d1e87a41582
ronnie bhatt <ronniebhatt@ronnies-MacBook-Air.local>
2019-04-02 13:28:46 +0530

    changed

--- src/components/DocumentLayouts.js
+++ src/components/DocumentLayouts.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 import {
-  Text,
-  Page,
-  View,
-  Document
+    Text,
+    Page,
+    View,
+    Document
 } from '@react-pdf/renderer'
 
 // @TODO i don't like this approach, but it looks simple for now
@@ -12,49 +12,33 @@ import {
 import MainLayout from './LayoutTypes/MainLayout'
 
 // import styles from './styles'
-import BulletListStyles from './LayoutStyles/BulletList'
-import RowViewStyles from './LayoutStyles/RowViewStyles'
-import ColumnViewStyles from './LayoutStyles/ColumnView'
+import { BulletList as BulletListStyles, ColumnView as ColumnViewStyles, RowView as RowViewStyles } from './LayoutTypes/LayoutStyles'
+//import BulletListStyles from './LayoutStyles/BulletList'
+//import RowViewStyles from './LayoutStyles/RowViewStyles'
+//import ColumnViewStyles from './LayoutStyles/ColumnView'
 
 // @TODO move away switch into a separated function
 
 const DocumentLayouts = (type, documentTitle, data) => {
-  let layout
+    let layout
 
-  switch (type) {
-    case 'PDF1':
-      layout = <MainLayout styles={BulletListStyles} data={data} />
-      break
+    type === 'PDF1' ? layout = <MainLayout styles={BulletListStyles} data={data} /> :
+        type === 'PDF2' ? layout = <MainLayout styles={RowViewStyles} data={data} /> :
+            type === 'PDF3' ? layout = <MainLayout styles={ColumnViewStyles} data={data} /> :
+                layout = <MainLayout styles={BulletListStyles} data={data} />
 
-    case 'PDF2':
-      layout = <MainLayout styles={RowViewStyles} data={data} />
-      break
-
-    case 'PDF3':
-      layout = <MainLayout styles={ColumnViewStyles} data={data} />
-      break
-
-    case 'PDF3':
-      layout = <MainLayout styles={ColumnViewStyles} data={data} />
-      break
-
-    default:
-      layout = <MainLayout styles={BulletListStyles} data={data} />
-      break
-  }
-
-  return (
-    <Document>
-      <Page size='A4' style={styles.page} wrap>
-        <View style={styles.section}>
-          <Text style={styles.text}>
-            {documentTitle}
-          </Text>
-          {layout}
-        </View>
-      </Page>
-    </Document>
-  )
+    return (
+        <Document>
+            <Page size='A4' style={styles.page} wrap>
+                <View style={styles.section}>
+                    <Text style={styles.text}>
+                        {documentTitle}
+                    </Text>
+                    {layout}
+                </View>
+            </Page>
+        </Document>
+    )
 }
 
 export default DocumentLayouts
