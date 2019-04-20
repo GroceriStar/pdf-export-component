1939be940b2b16cfcb76790614299fb4b491131a
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-04-20 12:09:00 +0300

    department list moved

---
+++ src/components/Layouts/LayoutItem/DepartmentLists.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import uuidv1 from 'uuid/v1'
+
+import { Text, View } from '@react-pdf/renderer'
+import List, { Item } from './LayoutItem/LayoutItem'
+
+const DepartmentLists = ({ department, ingredients, styles }) => (
+  <View style={styles.main}>
+    <Text style={styles.title}>
+      {department}
+    </Text>
+
+    <List>
+      {/* @TODO replace uuidv1 later */}
+      {ingredients.map((ingredient) =>
+        <Item key={uuidv1()}>{ingredient}</Item>)}
+    </List>
+  </View>
+)
+
+export default DepartmentLists
--- src/components/Layouts/MainLayout.js
+++ src/components/Layouts/MainLayout.js
@@ -2,29 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import uuidv1 from 'uuid/v1'
 
-import { Text, View } from '@react-pdf/renderer'
-import List, { Item } from './LayoutItem/LayoutItem'
-
-import BulletListStyles from './LayoutStyles/BulletList'
-import RowViewStyles from './LayoutStyles/RowView'
-import ColumnViewStyles from './LayoutStyles/ColumnView'
-
-// @TODO move this selector away from this not a container like component.
-// import { documentStyles as styles } from './styles'
-
-const ListsDepartment = ({ department, ingredients, styles }) => (
-  <View style={styles.main}>
-    <Text style={styles.title}>
-      {department}
-    </Text>
-
-    <List>
-      {/* @TODO replace uuidv1 later */}
-      {ingredients.map((ingredient) =>
-        <Item key={uuidv1()}>{ingredient}</Item>)}
-    </List>
-  </View>
-)
+import { View } from '@react-pdf/renderer'
+import DepartmentLists from 'LayoutItem/DepartmentLists'
 
 // @TODO this is a bad situation where we have a passing data as it is - we use names, not id at not a main container - component
 // and this will be hardly to catch later
@@ -55,7 +34,7 @@ const MainLayout = ({type, data }) => {
   return (
     <View>
       {data && data.map(item => (
-        <ListsDepartment
+        <DepartmentLists
           styles={styles}
           key={uuidv1()}
           department={item.department}
