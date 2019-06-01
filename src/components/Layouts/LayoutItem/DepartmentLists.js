30a5f7bde43f4e93e744b5528fb818d971887bae
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-06-01 10:49:49 +0300

    codefix

--- src/components/Layouts/LayoutItem/DepartmentLists.js
+++ src/components/Layouts/LayoutItem/DepartmentLists.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import uuidv1 from 'uuid/v1'
 
 import { Text, View } from '@react-pdf/renderer'
