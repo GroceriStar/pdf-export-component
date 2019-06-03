40f82c94a1ad91684d6a6bf8d8ffc7f34fcc20c8
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-06-03 15:52:40 +0300

    tests for DownloadPDFLink not wrote, all commented

--- src/components/DownloadLink/DownloadPDFLink.test.js
+++ src/components/DownloadLink/DownloadPDFLink.test.js
@@ -1,27 +1,19 @@
-/* globals describe, it, expect */
-
-import React from 'react'
-// import ReactDOM from 'react-dom'
-// import Enzyme, { shallow, configure } from 'enzyme'
-import { shallow, configure } from 'enzyme'
-
-import EnzymeAdapter from 'enzyme-adapter-react-16'
-
-import TodaysDate from '~/TodaysDate'
-
-// import DownloadPDFLink from './DownloadPDFLink'
-
-// Setup enzyme's react adapter
-configure({ adapter: new EnzymeAdapter() })
-
-describe('<TodaysDate /> component shallow only methods ', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<TodaysDate />)
-
-    // expect(wrapper).toBe({});
-    // this 2 lines below don't make a lot of sense
-
-    expect(wrapper).toBe(true)
-    //   ReactDOM.render(<List />, div);
-  })
-})
+// /* globals describe, it, expect */
+// import React from 'react'
+// // import ReactDOM from 'react-dom'
+// // import Enzyme, { shallow, configure } from 'enzyme'
+// import { shallow, configure } from 'enzyme'
+// import EnzymeAdapter from 'enzyme-adapter-react-16'
+// import TodaysDate from '~/TodaysDate'
+// // import DownloadPDFLink from './DownloadPDFLink'
+// // Setup enzyme's react adapter
+// configure({ adapter: new EnzymeAdapter() })
+// describe('<TodaysDate /> component shallow only methods ', () => {
+//   it('renders without crashing', () => {
+//     const wrapper = shallow(<TodaysDate />)
+//     // expect(wrapper).toBe({});
+//     // this 2 lines below don't make a lot of sense
+//     expect(wrapper).toBe(true)
+//     // ReactDOM.render(<List />, div);
+//   })
+// })
