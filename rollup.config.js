f14886517b6d5b4e41803143aabc6f6190dff45f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-16 07:44:54 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
-// import copy from 'rollup-plugin-copy-assets';
 // import replace from 'rollup-plugin-replace'
 // import uglify from 'rollup-plugin-uglify'
 
